d8ed021299362d8fc2b774781263cc8b1b812b0c
Kendra-ui <knokam72@gmail.com>
2023-10-11 13:43:12 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,32 +17,88 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+   final TextEditingController _controller = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {  
-    return const Scaffold(
+    return    Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
+
         child:SingleChildScrollView(
           child: Column(
             children: [
-              Padding(padding: EdgeInsets.all(10),
-              child: Text('Shopping List', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 45),
+               const Padding(padding: EdgeInsets.all(10),   
+              child: 
+              Text('Shopping List', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30),
               ),
-              ),
-            Padding(padding: EdgeInsetsDirectional.all(50),
-            child: 
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Title',
-              ),
-              ),
+              ), 
+
+              Padding( padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding( padding: EdgeInsets.all(5),
+                      child: SizedBox(
+                        width: 200,
+                        height: 40,
+                        child: Expanded(
+                          child: TextField(
+                          controller: _controller,
+                          decoration:  InputDecoration(
+                            hintText: 'Title...',
+                            filled: true,
+                            fillColor: Color.fromARGB(255, 197, 197, 197),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(2),
+                              borderSide: BorderSide.none
+                            )
+                          ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding( padding: EdgeInsets.all(5),
+                      child: SizedBox(
+                        width: 70,
+                        height: 40,
+                        child: Expanded(
+                          child: TextField(
+                          controller: _controller,
+                          decoration:  InputDecoration(
+                            hintText: 'Date...',
+                            filled: true,
+                            fillColor: Color.fromARGB(255, 216, 216, 216),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(2),
+                              borderSide: BorderSide.none
+                            )
+                          ),
+                          ),
+                        ),
+                      ),
+                    ),
+              
+                    Padding(padding: EdgeInsets.only(left:5),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        side: BorderSide(color: Colors.yellow)
+                         ),
+                        onPressed: () {},
+                         child: const Text('Add', style: TextStyle(color: Colors.yellow),),
+                                    ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
